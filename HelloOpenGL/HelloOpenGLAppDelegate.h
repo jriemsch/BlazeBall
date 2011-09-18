a5a6a9427839a5884a94f7ceee42dffc404e9b33
Jens Riemschneider <ky@jens-riemschneiders-macbook.local>
2011-09-18 18:15:32 +0200

    Initial Commit

---
+++ HelloOpenGL/HelloOpenGLAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HelloOpenGLAppDelegate.h
+//  HelloOpenGL
+//
+//  Created by Jens Riemschneider on 9/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloOpenGLAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
